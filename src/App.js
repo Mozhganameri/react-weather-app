0bdc083273afdc3088df0e9d671da12eca72e78d
Mozhgan Ameri <mozhganameri69@gmail.com>
2022-08-24 16:24:40 +0430

    added github repo url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+        <h1>Weather App</h1>
+        <footer>
+        This project was coded by Mozhgan Ameri and is{" "}
+      <a href="https://github.com/Mozhganameri/react-weather-app" target="_blank">Open-sourced </a>
+            on GitHub
+        </footer>
     </div>
   );
 }
